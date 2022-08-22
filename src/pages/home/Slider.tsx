1c550f260e477db412c96504cf837b15cf83b9ed
Komarov Alexander <sanrkom@gmail.com>
2022-08-23 00:12:05 +0200

    добавил картинки

--- src/pages/home/Slider.tsx
+++ src/pages/home/Slider.tsx
@@ -2,11 +2,20 @@ import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
+const IMAGE_HEIGHT = 500;
+
 function Slider() {
   return (
     <Carousel>
       <Carousel.Item>
-        <StaticImage src="../../images/slider/zakat2.png" style={{width: '100%'}} alt="Закат на Покровке" />
+        <StaticImage src="../../images/slider/zakat2.png" height={IMAGE_HEIGHT} style={{width: '100%'}}  quality={100} alt="Закат на Покровке" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <StaticImage src="../../images/slider/musey.jpg" height={IMAGE_HEIGHT} style={{width: '100%'}}  quality={100} alt="Музей" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
