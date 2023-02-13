7190b79a9dd571a07343098a296c7f782f2c7194
Kossskom <ya.kom345@yandex.ru>
2023-02-13 18:42:59 +0300

    Kerd1

--- src/pages/splavy/kerd1.tsx
+++ src/pages/splavy/kerd1.tsx
@@ -21,7 +21,7 @@ const events = [
   {
     end: '10.02.2023',
     start: '09.02.2023',
-    title: 'Сплав 2 дня'
+    title: '2-х дневный сплав забронирован. Осталось 6 мест на однодневный сплав'
   }];
 
 const calenderEvents = events.map(e => ({ title: e.title, start: moment(e.start, 'DD.MM.YYYY'), end: moment(e.end, 'DD.MM.YYYY').add(1, 'd')}))
@@ -32,93 +32,94 @@ const kerd1 = () => (
       <Carousel controls={false} indicators={false}>
         <Carousel.Item>
           <div>
-            <StaticImage  src="../images/Splav/Prony/14.png" className="rounded" alt="Пронские сплавы"/>
+            <StaticImage src="../images/Splav/Kerd/CollageKerd.png" className="rounded" alt="Пронские сплавы" style={{width: '100%', height: 'auto'}}/>
             <Carousel.Caption style={captionStyle}>
-            <h1>ПРОНСКИЕ СПЛАВЫ</h1>
+            <h1>1 День</h1>
             </Carousel.Caption>
           </div>
         </Carousel.Item>
       </Carousel>
     </div>
-    <Calendar
-      localizer={localizer}
-      culture={'ru'}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 500 }}
-      views={['month']}
-      events={calenderEvents}
-      messages={ {
-        previous: 'Предыдущий месяц',
-        next: 'Следующий месяц',
-        today: `Сегодня`,
-      }}
-    />
 
     <div className="container text" style={{paddingTop: 50}}>
       <h2 style={{textAlign: 'center'}}>Кердь</h2>
-      <p style={{fontSize: '18px', textAlign: "justify"}}>Это не большая по ширине речка, извилистая и неглубокая, а скорость течения вполне умеренная. Во время сплава вас будет окружать пение птиц и полеты бабочек. Данный маршрут (протяжённостью ≈ 18 км)  подходит всем желающим, включая новичков. Взрослый могут взять с собой детей, которым исполнилось уже 6 лет.</p>
-      <p style={{fontSize: '18px', textAlign: "justify"}}>Стоимость 1-дневного сплава 2000 р., для детей до 18 лет скидка 1000р. Входит:<br />
-      - байдарки в собранном виде, спасжилеты, в т.ч. детские</p>
-      <p style={{fontSize: '18px', textAlign: "justify"}}>Стоимость 1-дневного сплава 2000 р., для детей до 18 лет скидка 1000р. Входит:</p>
-      <p style={{fontSize: '18px', textAlign: "justify"}}>Стоимость 1-дневного сплава 2000 р., для детей до 18 лет скидка 1000р. Входит:</p>
-      <p style={{fontSize: '18px', textAlign: "justify"}}>Стоимость 1-дневного сплава 2000 р., для детей до 18 лет скидка 1000р. Входит:</p>
-      <i style={{fontSize: '18px', textAlign: "justify"}}>Бронирование мест по телефону 89521233539.</i>
-      <i style={{fontSize: '18px', textAlign: "justify"}}>Бронирование мест по телефону 89521233539.</i>
+      <p style={{fontSize: '18px', textAlign: "justify"}}>Это не большая по ширине речка, извилистая и неглубокая, а скорость течения вполне умеренная. 
+      Во время сплава вас будет окружать тишина, нарушаемая только пение птиц, полеты бабочек, приятная прохлада от реки и тень от деревьев. 
+      В этом месте вы точно почувствуете уединение с природой и перезагрузитесь от городского ритма и суеты. 
+      Данный маршрут (протяжённостью ≈ 18 км)  подходит всем желающим, включая новичков. 
+      Взрослый могут взять с собой детей, которым исполнилось уже 6 лет. Байдарки у нас пластиковые очень надежные и устойчивые.</p>
+      <i style={{fontSize: '18px', textAlign: "justify"}}>Стоимость 1-дневного сплава 2000 р., для детей до 18 лет скидка 1000р. Входит:<br />
+      - байдарки в собранном виде<br />
+      - спасжилеты, в т.ч. детские<br />
+      - гермомешки<br />
+      - туристические коврики<br />
+      - трансфер к месту старта и обратно<br /></i>
+      <b style={{fontSize: '18px', textAlign: "justify"}}>Для бронирования и по всем возникающим вопросам звоните или пишите в мессенджер по телефону 8(952)123-35-39 (c 10:00 до 20:00).</b>
     </div>
-  
-
 
     <div className="container text" style={{paddingTop: 50}}>
-      <div className="row">
-        <div className="col">
-          <div className="card">
-            <StaticImage width={600}  src="../images/Splav/Kerd/1.png" className="card-img-top" alt="Кердь" style={{width: '100%', height: 'auto'}}/>
-            <div className="card-body">
-              <h5 className="card-title">1 День </h5>
-              <p className="card-text">Цена 2000р., детям до 18 скидка 1000р.</p>
-              <a href="#" className="btn btn-primary">Подробнее</a>
-            </div>
-          </div> 
-        </div>
-        <div className="col">
-          <div className="card">
-            <StaticImage width={600}  src="../images/Splav/Kerd/2.jpg" className="card-img-top" alt="Кердь" style={{width: '100%', height: 'auto'}}/>
-            <div className="card-body">
-              <h5 className="card-title">1 День / 1 Ночь</h5>
-              <p className="card-text">Цена 3000р., детям до 18 скидка 1000р.</p>
-              <a href="#" className="btn btn-primary">Подробнее</a>
-            </div>
-          </div> 
-        </div>
-        <div className="col">
-          <div className="card">
-            <StaticImage width={600}  src="../images/Splav/Prony/3-1.jpg" className="card-img-top" alt="Проня" style={{width: '100%', height: 'auto'}}/>
-            <div className="card-body">
-              <h5 className="card-title">2 Дня / 1 Ночь</h5>
-              <p className="card-text">Цена 4000р., детям до 18 скидка 1000р.</p>
-              <a href="#" className="btn btn-primary">Подробнее</a>
-            </div>
-          </div> 
-        </div>
-      </div>
+      <i style={{fontSize: '18px', textAlign: "justify"}}>* Бронирование производится по 50% предоплате, предварительно можете ознакомиться с графиком занятости путешествий.
+      В случае отказа за 3 дня до мероприятия, предоплата возвращается в полном объеме.</i>
+      <Calendar
+        localizer={localizer}
+        culture={'ru'}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        views={['month']}
+        events={calenderEvents}
+        messages={ {
+          previous: 'Предыдущий',
+          next: 'Следующий',
+          today: `Сегодня`,
+        }}
+      />
+    </div>
+  
+    <div className="container text" style={{paddingTop: 50}}>
+      <b style={{fontSize: '18px', textAlign: "justify"}}>Примерная программа однодневного сплава:</b>
+      <p style={{fontSize: '18px', textAlign: "justify"}}>
+      10:30 – прибытие в г.Пронск к нашему дому, трансфер на место старта. Добраться до города можно на общественном транспорте или на собственном автомобиле, припаркуем его у нашего дома<br />
+      11:00 - 11:30 - инструктаж, получение снаряжение, загрузка байдарок, старт<br />
+      11:30 - 17:00 - сплав на байдарках с остановками для фотографирования и купания<br />
+      17:00 - финиширование у Пронского моста, обратный трансфер к дому
+      </p>
+    </div>
+
+    <div className="container text" style={{paddingTop: 50}}>
+      <h3 style={{textAlign: 'center'}}>Что взять с собой в поход</h3>
+      <b style={{fontSize: '18px', textAlign: "justify"}}>Питьевая вода</b>
+      <p style={{fontSize: '18px', textAlign: "justify"}}>в удобной бутылке или нескольких маленьких</p>
+      <b style={{fontSize: '18px', textAlign: "justify"}}>Головной убор и солнце защитные очки</b>
+      <p style={{fontSize: '18px', textAlign: "justify"}}>уберегут вас от солнечных лучей</p>
+      <b style={{fontSize: '18px', textAlign: "justify"}}>Крем</b>
+      <p style={{fontSize: '18px', textAlign: "justify"}}>крем от загара пригодится вам если вы склонны к обгоранию кожи на солнце</p>
+      <b style={{fontSize: '18px', textAlign: "justify"}}>Бутерброды, фрукты, овощи</b>
+      <p style={{fontSize: '18px', textAlign: "justify"}}>на воде тратится много калорий, так что перекусить точно захочется</p>
+      <b style={{fontSize: '18px', textAlign: "justify"}}>Обувь в байдарку</b>
+      <p style={{fontSize: '18px', textAlign: "justify"}}>сланцы, кроксы</p>
     </div>
 
     <div className="container text" style={{paddingTop: 50, paddingBottom: 50}}>
       <Carousel>
         <Carousel.Item>
           <div>
-            <StaticImage  src="../images/Splav/Prony/C1.png" className="rounded" alt="Кердь"/>
+          <iframe width='100%' height='700' src="https://www.youtube.com/embed/A9zsM14ONlI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
           </div>
         </Carousel.Item>
         <Carousel.Item>
           <div>
-            <StaticImage  src="../images/Splav/Kerd/1.png" className="rounded" alt="Кердь"/>
+            <StaticImage  src="../images/Splav/Prony/13.png" className="rounded" alt="Кердь"/>
           </div>
         </Carousel.Item>
         <Carousel.Item>
           <div>
-            <StaticImage  src="../images/Splav/Prony/4.jpg" className="rounded" alt="Проня"/>
+            <StaticImage  src="../images/Splav/Prony/14.png" className="rounded" alt="Кердь"/>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div>
+            <StaticImage  src="../images/Splav/Prony/14.png" className="rounded" alt="Проня"/>
           </div>
         </Carousel.Item>
       </Carousel>
