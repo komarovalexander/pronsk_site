e84996c1fc294f4262332e29cfd157bbb212fdee
Kossskom <ya.kom345@yandex.ru>
2023-02-17 11:19:51 +0300

    Кердь готова

--- src/pages/splavy/kerd1.tsx
+++ src/pages/splavy/kerd1.tsx
@@ -19,51 +19,64 @@ const captionStyle: CSSProperties = {
 };
 const events = [
   {
-    end: '10.02.2023',
+    end: '09.02.2023',
     start: '09.02.2023',
-    title: '2-х дневный сплав забронирован. Осталось 6 мест на однодневный сплав'
-  }];
-
+    title: '2-дневный сплав'
+  },
+  {
+    end: '09.02.2023',
+    start: '09.02.2023',
+    title: '1-дневный сплав свободно 6 мест'
+  },
+  {
+    end: '09.02.2023',
+    start: '09.02.2023',
+    title: '1-дневный сплав свободно 6 мест'
+  },];
 const calenderEvents = events.map(e => ({ title: e.title, start: moment(e.start, 'DD.MM.YYYY'), end: moment(e.end, 'DD.MM.YYYY').add(1, 'd')}))
 
 const kerd1 = () => (
   <Layout>
-    <div className="container text" style={{paddingTop: 50}}>
+    <div className="container text" style={{paddingTop: 30}}>
       <Carousel controls={false} indicators={false}>
         <Carousel.Item>
           <div>
             <StaticImage src="../images/Splav/Kerd/CollageKerd.png" className="rounded" alt="Пронские сплавы?"/>
             <Carousel.Caption style={captionStyle}>
-            <h1>1 День</h1>
+            <h1>Кердь-Проня (Дневной сплав)</h1>
             </Carousel.Caption>
           </div>
         </Carousel.Item>
       </Carousel>
     </div>
 
-    <div className="container text" style={{paddingTop: 50}}>
-      <h2 style={{textAlign: 'center'}}>Кердь</h2>
-      <p style={{fontSize: '18px', textAlign: "justify"}}>Это не большая по ширине речка, извилистая и неглубокая, а скорость течения вполне умеренная. 
-      Во время сплава вас будет окружать тишина, нарушаемая только пение птиц, полеты бабочек, приятная прохлада от реки и тень от деревьев. 
-      В этом месте вы точно почувствуете уединение с природой и перезагрузитесь от городского ритма и суеты. 
+    <div className="container text" style={{paddingTop: 30, paddingBottom: 30, backgroundColor: '#eef0f2'}}>
+      <p style={{fontSize: '18px', textAlign: "justify"}}>Кердь - это не большая по ширине речка, извилистая и неглубокая, а скорость течения на ней вполне умеренная. 
+      Во время сплава вас будет окружать пение птиц, шелест листьев, журчание ручья, приятная прохлада от реки и тень от деревьев. 
+      Вокруг много живности, рядом летают бабочки, плавают утки, пасутся коровки.
+      В этом месте вы точно почувствуете уединение с природой и отдохнете от городского ритма и суеты. 
       Данный маршрут (протяжённостью ≈ 18 км)  подходит всем желающим, включая новичков. 
       Взрослый могут взять с собой детей, которым исполнилось уже 6 лет. Байдарки у нас пластиковые очень надежные и устойчивые.</p>
-      <i style={{fontSize: '18px', textAlign: "justify"}}>Стоимость 1-дневного сплава 2000 р., для детей до 18 лет скидка 1000р. Входит:<br />
-      - байдарки в собранном виде<br />
-      - спасжилеты, в т.ч. детские<br />
-      - гермомешки<br />
-      - туристические коврики<br />
-      - трансфер к месту старта и обратно<br /></i>
+      <i style={{fontSize: '18px', textAlign: "justify"}}>Стоимость 1-дневного сплава 2000 р.(от 2-х человек), для детей до 18 лет скидка 1000р. Входит:<br />
+      - байдарки в собранном виде<br/>
+      - спасжилеты, в т.ч. детские<br/>
+      - гермомешки<br/>
+      - туристические коврики<br/>
+      - трансфер к месту старта и обратно<br/></i>
       <b style={{fontSize: '18px', textAlign: "justify"}}>Для бронирования и по всем возникающим вопросам звоните или пишите в мессенджеры по телефону 8(952)123-35-39 (c 10:00 до 20:00).</b>
     </div>
 
-    <div className="container text" style={{paddingTop: 50}}>
-      <i style={{fontSize: '18px', textAlign: "justify"}}>* Бронирование производится по 50% предоплате, предварительно можете ознакомиться с графиком занятости путешествий.
+    <div className="container text" style={{paddingBottom: 30}}>
+      <h3 style={{textAlign: 'center'}}>График занятости путешествий</h3>
+      <i style={{fontSize: '18px', textAlign: "justify"}}>* Бронирование производится по 30% предоплате.
       В случае отказа за 3 дня до мероприятия, предоплата возвращается в полном объеме.</i>
       <Calendar
         localizer={localizer}
         culture={'ru'}
         startAccessor="start"
+        onSelectEvent={(e) => {
+console.log(e);
+        }}
         endAccessor="end"
         style={{ height: 500 }}
         views={['month']}
@@ -76,17 +89,17 @@ const kerd1 = () => (
       />
     </div>
   
-    <div className="container text" style={{paddingTop: 50}}>
+    <div className="container text">
       <b style={{fontSize: '18px', textAlign: "justify"}}>Примерная программа однодневного сплава:</b>
       <p style={{fontSize: '18px', textAlign: "justify"}}>
-      10:30 – прибытие в г.Пронск к нашему дому, трансфер на место старта. Добраться до города можно на общественном транспорте или на собственном автомобиле, припаркуем его у нашего дома (от Рязани до нас 30-40 минут езды)<br />
-      11:00 - 11:30 - инструктаж, получение снаряжение, загрузка байдарок, старт<br />
-      11:30 - 17:00 - сплав на байдарках с остановками для фотографирования и купания<br />
+      10:30 – прибытие в г.Пронск к нашему дому, трансфер на место старта.<br />
+      11:00 - инструктаж, получение снаряжение, загрузка байдарок, старт<br />
+      11:30 - сплав на байдарках с остановками для фотографирования и купания<br />
       17:00 - финиширование у Пронского моста, обратный трансфер к дому
       </p>
     </div>
 
-    <div className="container text" style={{paddingTop: 50}}>
+    <div className="container text" style={{paddingTop: 30, paddingBottom: 30, backgroundColor: '#eef0f2'}}>
       <h3 style={{textAlign: 'center'}}>Что взять с собой в поход</h3>
       <b style={{fontSize: '18px', textAlign: "justify"}}>Питьевая вода</b>
       <p style={{fontSize: '18px', textAlign: "justify"}}>в удобной бутылке или нескольких маленьких</p>
@@ -100,31 +113,56 @@ const kerd1 = () => (
       <p style={{fontSize: '18px', textAlign: "justify"}}>обувь в байдарку (сланцы, кроксы), обувь на стоянки (кроссовки, кеды), головной убор, солнце защитные очки</p>
     </div>
 
-    <div className="container text" style={{paddingTop: 50, paddingBottom: 50}}>
+    <div className="container text" style={{paddingBottom: 30}}>
       <Carousel>
         <Carousel.Item>
           <div>
-          <iframe width='100%' height='730' src="https://www.youtube.com/embed/A9zsM14ONlI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+            <StaticImage  src="../images/Splav/Kerd/1.png" className="rounded" alt="Кердь"/>
           </div>
         </Carousel.Item>
+        <Carousel.Item>
+          <div>
+            <StaticImage  src="../images/Splav/Kerd/2.png" className="rounded" alt="Пороня"/>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div>
+            <StaticImage  src="../images/Splav/Kerd/3.png" className="rounded" alt="Проня"/>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div>
+            <StaticImage  src="../images/Splav/Kerd/4.png" className="rounded" alt="Кердь"/>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div>
+            <StaticImage  src="../images/Splav/Kerd/5.png" className="rounded" alt="Кердь"/>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div>
+            <StaticImage  src="../images/Splav/Kerd/6.png" className="rounded" alt="Кердь"/>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div>
+            <StaticImage  src="../images/Splav/Kerd/7.png" className="rounded" alt="Кердь"/>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div>
+            <StaticImage  src="../images/Splav/Kerd/8.png" className="rounded" alt="Кердь"/>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+
+    <div className="container text" style={{paddingTop: 50, paddingBottom: 50}}>
+      <Carousel controls={false} indicators={false}>
         <Carousel.Item>
           <div>
           <iframe width='100%' height='730' src="https://www.youtube.com/embed/JOSEilZ7slQ" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div>
-            <StaticImage  src="../images/Splav/Prony/13.png" className="rounded" alt="Кердь"/>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div>
-            <StaticImage  src="../images/Splav/Prony/14.png" className="rounded" alt="Кердь"/>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div>
-            <StaticImage  src="../images/Splav/Prony/14.png" className="rounded" alt="Проня"/>
           </div>
         </Carousel.Item>
       </Carousel>
