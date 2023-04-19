887c3a4c9ef0bb096063d4880c3f9a50ab91fc58
Alexander Komarov <k.alex@qvedo.com>
2023-04-19 22:47:12 +0200

    add metrika

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,26 +1,45 @@
 import '../table/ka-bootstrap.scss';
 
-import { HeadFC } from 'gatsby';
 import * as React from 'react';
 
-import Slider from './home/Slider';
+import { HeadFC } from 'gatsby';
 import Navigation from './Navigation';
+import Slider from './home/Slider';
 
 const pageStyles = {
-  color: "#232129",
-  padding: "50px 0",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+  color: '#232129',
+  padding: '50px 0',
+  fontFamily: '-apple-system, Roboto, sans-serif, serif',
+};
 
-const Layout =({ children }) => {
+const Layout = ({ children }) => {
+  React.useEffect(() => {
+    appendMetrika();
+  }, []);
   return (
     <>
       <Navigation />
-        {children}
+      {children}
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
 
-export const Head: HeadFC = () => <title>В Пронске - Народный сайт Пронска и Пронского района</title>
+const appendMetrika = () => {
+  const result = document.createElement('script');
+  result.innerHTML = `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+  ym(91393419, "init", {
+       clickmap:true,
+       trackLinks:true,
+       accurateTrackBounce:true,
+       webvisor:true
+  });`;
+  document.body.appendChild(result);
+};
+export const Head: HeadFC = () => <title>В Пронске - Народный сайт Пронска и Пронского района</title>;
