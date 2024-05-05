72a942972ac87f35b57d29343cbc00426dd1af58
Alexander Komarov <k.alex@qvedo.com>
2024-05-05 17:09:23 +0300

    update navigation step 1

--- src/pages/Navigation.tsx
+++ src/pages/Navigation.tsx
@@ -1,48 +1,25 @@
-import Link from 'gatsby-link';
-import { StaticImage } from 'gatsby-plugin-image';
 import * as React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import { StaticImage } from 'gatsby-plugin-image';
+import { links } from './Navigation.module.scss';
 
 const Navigation = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="/" title="В Пронске - Народный сайт Пронска и Пронского района" style={{
-            display: 'flex',
-            justifyItems: 'center',
-            justifyContent: 'center',
-            alignItems: 'center',
-            gap: 10
-        }}>
-          <StaticImage src="../images/герб.png" alt="В Пронске" width={50} style={{ marginLeft: 2 }}/>
-          <div style={{
-            fontWeight: 'bold'
-            }}>В Пронске</div></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Главная</Nav.Link>
-           {/* <Nav.Link href="/memorials/">Памятники</Nav.Link>*/}
-            <Nav.Link href="/splavy/">Пронские сплавы</Nav.Link>
-            {/*<NavDropdown title="Жителям" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-          </NavDropdown>*/}
-            <Nav.Link href="/blog/">Блог</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div style={{ display: 'flex', alignItems: 'center', gap: 10, padding: 20 }}>
+      <div style={{ display: 'flex', alignItems: 'center', gap: 10, padding: 20 }}>
+        <StaticImage src="../images/герб.png" alt="В Пронске" width={50} style={{ marginLeft: 2 }} />
+        <div style={{
+          fontWeight: 'bold',
+          fontSize: 32,
+          fontFamily: 'molodo'
+        }}>В Пронске</div>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', gap: 30, padding: 20 }} className={links}>
+        <a href="/">Главная</a>
+        <a href="/splavy/">Пронские сплавы</a>
+        <a href="/blog/">Блог</a>
+      </div>
+    </div >
   );
 }
 
