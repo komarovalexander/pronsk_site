5ef633f5442a89970fc82dc07db326809ff8d18a
Alexander Komarov <k.alex@qvedo.com>
2024-05-07 10:43:17 +0300

    update menu

--- src/pages/Navigation.tsx
+++ src/pages/Navigation.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 
-import { container, links } from './Navigation.module.scss';
+import { container, header, links, linksMenu, menuButton } from './Navigation.module.scss';
 
 import gerbImage from '../images/герб.png';
+import menuImage from '../images/menu.svg';
 
 const Navigation = () => {
+  const [menuShown, setMenuShown] = React.useState(false);
   const activeItem = location.pathname.split('/')?.[1];
   return (
-    <div className={container} style={{ display: 'flex', alignItems: 'center', gap: 10 }}>
+    <div className={`${container} ${header}`}>
       <a href="/" style={{
         textDecoration: 'none',
         color: '#4D3E37'
@@ -19,11 +21,23 @@ const Navigation = () => {
             fontSize: 32,
             fontFamily: 'molodo',
           }}>В Пронске</div>
-        </div> </a>
-      <div style={{ display: 'flex', alignItems: 'center', gap: 30, padding: 20 }} className={links}>
+        </div>
+      </a>
+      <div className={links}>
         <a className={activeItem === '' ? 'active' : undefined} href="/">Главная</a>
         <a className={activeItem === 'splavy' ? 'active' : undefined} href="/splavy/">Пронские сплавы</a>
         <a className={activeItem === 'blog' ? 'active' : undefined} href="/blog/">Блог</a>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', gap: 30, padding: 20 }} className={linksMenu}>
+        <button className={menuButton} onClick={() => { setMenuShown(!menuShown); }}>
+          <img src={menuImage} alt="Меню" height={30} width={30} />
+        </button>
+
+        <div className='linksMenuList' style={menuShown ? {} : { display: 'none' }}>
+          <a className={activeItem === '' ? 'active' : undefined} href="/">Главная</a>
+          <a className={activeItem === 'splavy' ? 'active' : undefined} href="/splavy/">Пронские сплавы</a>
+          <a className={activeItem === 'blog' ? 'active' : undefined} href="/blog/">Блог</a>
+        </div>
       </div>
     </div >
   );
