f900f9e8b5101b84594a62706d1c7cded534aaef
Alexander Komarov <k.alex@qvedo.com>
2024-05-07 11:31:52 +0300

    add main banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,37 +3,36 @@ import 'moment/locale/ru'; // without this line it didn't work
 
 import * as React from 'react';
 
-import Container from 'react-bootstrap/esm/Container';
+import {
+  mainBanner,
+  mainBannerImage,
+  mainBannerText,
+  mainBannerText1,
+  mainBannerText2,
+  mainBannerText3,
+  mainBannerText3P
+} from './index.module.scss';
+
 import { HeadFC } from 'gatsby';
 import Layout from './layout';
-import Slider from './home/Slider';
-
-const pageStyles = {
-  color: "#232129",
-  padding: "50px 0",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
-  const [expanded, setExpanded] = React.useState(false);
   return (
     <Layout>
-      <Slider />
-      <main style={pageStyles} className='content'>
-        <Container style={{ marginBottom: 50 }}>
-          <h3>Бесплатный прокат коньков</h3>
-          <p>
-            Коньки можно взять под роспись бесплатно в <a href="http://prdk.rzn.muzkult.ru/about">Пронском Районном Доме Культуры</a>.
-          </p>
+      <div className={mainBanner} >
+        <StaticImage className={mainBannerImage} src={'https://sun9-7.userapi.com/impg/Dlsl9k0WQ1p62E4sLwfA1kh7fthIPOTBiSgPwA/VK2fvLBqZpY.jpg?size=2560x1707&quality=95&sign=4a3fbdf90ad21c77d72967c2159a09e0&type=album'} alt={'Пронские сплавы '} />
+        <div style={{ position: 'absolute', zIndex: 1, height: '100%', width: '100%' }}>
 
-          <p>
-            Просьба возвращать коньки сразу после катания. В случае если после катания ДК уже закрыт - можно вернуть их сразу после открытия ДК.
-          </p>
-        </Container>
-        <Container>
-          <h3>Ближайщие мероприятия в Пронском районе <a href="http://prdk.rzn.muzkult.ru/egemesechnuplan">http://prdk.rzn.muzkult.ru/egemesechnuplan</a></h3>
-        </Container>
-      </main>
+          <div className={mainBannerText}>
+            <div className={`${mainBannerText1}`}>Добро пожаловать</div>
+            <div className={`${mainBannerText2}`} >В Пронск</div>
+            <div className={`${mainBannerText3} d-flex justify-content-center`}>
+              <p className={mainBannerText3P}>Город с красивейшей природой, богатой историей, и приветливыми людьми</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
