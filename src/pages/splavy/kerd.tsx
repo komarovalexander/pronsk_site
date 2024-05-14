df0e4e8953dcddb34e33d3cafb98ebed83df248a
Alexander Komarov <k.alex@qvedo.com>
2024-05-14 14:01:32 +0300

    удалил бутстрап из однодневного

--- src/pages/splavy/kerd.tsx
+++ src/pages/splavy/kerd.tsx
@@ -1,9 +1,64 @@
-import { Carousel, Col, Container, Modal, Row } from 'react-bootstrap';
 import React, { CSSProperties } from 'react';
+import {
+  additionalFeature,
+  additionalFeatureHeader,
+  button,
+  buttonMain,
+  buttonRoutes,
+  buttons,
+  columnReverseMobile,
+  container,
+  darkBack,
+  feature,
+  featureHeader,
+  featureNumber,
+  feedback,
+  feedbackItem,
+  feedbackItemDoubleQuote,
+  feedbackItemText,
+  feedbackLink,
+  feedbackPerson,
+  feedbackPersonDate,
+  feedbackPersonImage,
+  feedbackPersonName,
+  footer,
+  footerLinks,
+  grayBack,
+  header,
+  imageContainer,
+  linearBack,
+  logo,
+  mainBanner,
+  mainBannerText,
+  mainBannerText1,
+  mainBannerText2,
+  mainBannerText2Br,
+  mainBannerText3,
+  mainBannerText3P,
+  partner,
+  partnerRow,
+  row,
+  rowHeader,
+  rowImage,
+  rowImageBigHeight,
+  rowReversed,
+  rowText,
+  social,
+  tinyRow,
+  triangle,
+  underlinedLink,
+  video,
+  videoContainer,
+} from '../splavy.module.scss';
 
 import { HeadFC } from 'gatsby';
 import Layout from '../layout';
 import { StaticImage } from 'gatsby-plugin-image';
+import logoImage from '../../images/Splav/logo.png';
+import odnoklassniki from '../../images/odnoklassniki.svg';
+import telegram from '../../images/telegram.svg';
+import vk from '../../images/vk.svg';
+import whatsapp from '../../images/whatsapp.svg';
 
 const captionStyle: CSSProperties = {
   padding: '10px 30px',
@@ -14,85 +69,85 @@ const kerd = () => {
 
   return (
     <Layout>
-      <Container>
+      <div className={container}>
         <nav aria-label="breadcrumb" style={{ marginTop: 10 }}>
           <ol className="breadcrumb">
             <li className="breadcrumb-item"><a href="/splavy/">Пронские сплавы</a></li>
             <li className="breadcrumb-item active" aria-current="page">Однодневный сплав, реки: Кердь и Проня</li>
           </ol>
         </nav>
-      </Container>
+      </div>
 
-      <Row style={{ padding: '40px 0' }}>
-        <Col>
-          <div className="container text">
-            <Carousel controls={false} indicators={false}>
-              <Carousel.Item>
-                <div>
-                  <StaticImage src="../../images/Splav/Prony/1day.jpg" className="rounded" alt="Пронские сплавы?" />
-                  <Carousel.Caption style={captionStyle}>
-                    <h1>Однодневный сплав, реки: Кердь и Проня</h1>
-                  </Carousel.Caption>
-                </div>
-              </Carousel.Item>
-            </Carousel>
+
+      <div className={mainBanner} >
+        <StaticImage style={{ position: 'absolute', zIndex: 0, height: '100%', width: '100%' }} src={'../../images/Splav/Prony/1day.jpg'} alt={'Пронские сплавы '} />
+        <div style={{ position: 'absolute', zIndex: 1, height: '100%', width: '100%' }}>
+
+          <div className={mainBannerText}>
+            <div className={`${mainBannerText1} headerFont`}>
+              Однодневный сплав, реки: Кердь и Проня</div>
+            <div className={`${buttons} ${columnReverseMobile}`}>
+              <div style={{ display: 'flex', gap: 15, fontSize: 18, alignItems: 'center' }}>Пишите:
+                <a href="https://vk.me/pronskie_splavy" target='_blank' title='ВКонтакте'><img src={vk} alt='ВКонтакте' /></a>
+                <a href="https://t.me/pronskie_splavy" target='_blank' title='Телеграм'><img src={telegram} alt='Телеграм' /></a>
+                <a href="https://wa.me/+79521233539" target='_blank' title='Вотсапп'><img src={whatsapp} alt='Вотсапп' /></a>
+              </div>
+              <a className={buttonMain} href='tel:+79521233539'>Звоните +79521233539</a>
+            </div>
           </div>
-        </Col>
-      </Row>
+        </div>
+      </div>
 
-      <Row style={{ backgroundColor: '#eef0f2', padding: '40px 0' }}>
-        <Col>
-          <div className="container text">
-            <h3 style={{ textAlign: 'center' }}>🚣‍♂️ Особеность маршрута</h3>
-            <p style={{ fontSize: '18px', textAlign: "justify" }}>✨ Сервис "все включено": Наслаждайтесь беззаботным приключением с нашим полным сервисом,
-              из необходимого вам понадобится только питьевая вода в удобной бутылке.
-            </p><p style={{ fontSize: '18px', textAlign: "justify" }}>🍲 Кулинарное волшебство на берегу реки: Наш опытный походный повар готовит для вас изысканные блюда прямо на берегу, добавляя вкус к вашему приключению.
-            </p>
-            <p style={{ fontSize: '18px', textAlign: "justify" }}>👨‍👩‍👧‍👦 Для всей семьи: Наши походы подходят для любого уровня подготовки.
-              Пригласите семью и друзей. Для детей у нас большие скидки!
-            </p>
-            <p style={{ fontSize: '18px', textAlign: "justify" }}>👨‍🎓 Опытные гиды: Наши профессиональные гиды знают каждый поворот реки, безопасное и захватывающее приключение.
-            </p>
-            <p style={{ fontSize: '18px', textAlign: "justify" }}>🛶 Качественное снаряжение: Мы предоставляем современные и надежные байдарки, чтобы ваше путешествие было комфортным и беззаботным.
-            </p>
-            <p style={{ fontSize: '18px', textAlign: "justify" }}>🐦 Обилие животных: Во время сплава по реке Кердь можно насладиться пением птиц, шелестом листьев и журчанием ручья.
-              Вокруг много живности, таких как стрекозы (занесенные в красную книгу), утки, коровки и барашки, что создает удивительную атмосферу встречи с дикой природой.
-            </p>
-            <p style={{ fontSize: '18px', textAlign: "justify" }}>🌿 Отправляйтесь на неповторимое приключение по реке Кердь!🌊
-            </p>
-            <i style={{ fontSize: '18px', textAlign: "justify" }}>Стоимость 1-дневного сплава 3500₽ с человека, детям до 14 лет 2000₽ Входит:<br />
-              - байдарки в собранном виде<br />
-              - сопровождение<br />
-              - спасжилеты, в т.ч. детские<br />
-              - гермомешки<br />
-              - трансфер с Пронска к месту старта и обратно<br />
-              - полноценный обед<br /></i>
-            <b style={{ fontSize: '18px', textAlign: "justify" }}>Для бронирования и по всем возникающим вопросам звоните или пишите в мессенджеры по телефону <a href="tel:+79521233539">8(952)123-35-39</a> (<a href="https://vk.com/pronskie_splavy">Мы ВКонтакте</a>).<br /></b>
-            <i style={{ fontSize: '18px', textAlign: "justify" }}>*Бронирование производится по 50% предоплате.
-              В случае отказа за 7 дней до мероприятия, предоплата возвращается в полном объеме.</i>
-          </div>
-        </Col>
-      </Row>
+      <div className={container}>
+        <div className="container text">
+          <h3 style={{ textAlign: 'center' }}>🚣‍♂️ Особеность маршрута</h3>
+          <p style={{ fontSize: '18px', textAlign: "justify" }}>✨ Сервис "все включено": Наслаждайтесь беззаботным приключением с нашим полным сервисом,
+            из необходимого вам понадобится только питьевая вода в удобной бутылке.
+          </p><p style={{ fontSize: '18px', textAlign: "justify" }}>🍲 Кулинарное волшебство на берегу реки: Наш опытный походный повар готовит для вас изысканные блюда прямо на берегу, добавляя вкус к вашему приключению.
+          </p>
+          <p style={{ fontSize: '18px', textAlign: "justify" }}>👨‍👩‍👧‍👦 Для всей семьи: Наши походы подходят для любого уровня подготовки.
+            Пригласите семью и друзей. Для детей у нас большие скидки!
+          </p>
+          <p style={{ fontSize: '18px', textAlign: "justify" }}>👨‍🎓 Опытные гиды: Наши профессиональные гиды знают каждый поворот реки, безопасное и захватывающее приключение.
+          </p>
+          <p style={{ fontSize: '18px', textAlign: "justify" }}>🛶 Качественное снаряжение: Мы предоставляем современные и надежные байдарки, чтобы ваше путешествие было комфортным и беззаботным.
+          </p>
+          <p style={{ fontSize: '18px', textAlign: "justify" }}>🐦 Обилие животных: Во время сплава по реке Кердь можно насладиться пением птиц, шелестом листьев и журчанием ручья.
+            Вокруг много живности, таких как стрекозы (занесенные в красную книгу), утки, коровки и барашки, что создает удивительную атмосферу встречи с дикой природой.
+          </p>
+          <p style={{ fontSize: '18px', textAlign: "justify" }}>🌿 Отправляйтесь на неповторимое приключение по реке Кердь!🌊
+          </p>
+          <i style={{ fontSize: '18px', textAlign: "justify" }}>Стоимость 1-дневного сплава 3500₽ с человека, детям до 14 лет 2000₽ Входит:<br />
+            - байдарки в собранном виде<br />
+            - сопровождение<br />
+            - спасжилеты, в т.ч. детские<br />
+            - гермомешки<br />
+            - трансфер с Пронска к месту старта и обратно<br />
+            - полноценный обед<br /></i>
+          <b style={{ fontSize: '18px', textAlign: "justify" }}>Для бронирования и по всем возникающим вопросам звоните или пишите в мессенджеры по телефону <a href="tel:+79521233539">8(952)123-35-39</a> (<a href="https://vk.com/pronskie_splavy">Мы ВКонтакте</a>).<br /></b>
+          <i style={{ fontSize: '18px', textAlign: "justify" }}>*Бронирование производится по 50% предоплате.
+            В случае отказа за 7 дней до мероприятия, предоплата возвращается в полном объеме.</i>
+        </div>
+      </div>
 
 
-      <Row style={{ backgroundColor: '#eef0f2', padding: '40px 0' }}>
-        <Col>
-          <div className="container text">
-            <h3 style={{ textAlign: "center" }}>Примерная программа экспресс-сплава</h3>
-            <p style={{ fontSize: '18px', textAlign: "justify" }}>
-              10:00 – прибытие в р.п. Пронск в Глемпинг берег, трансфер на место старта.<br />
-              10:30 - инструктаж, получение снаряжение, загрузка байдарок, старт<br />
-              11:00 - сплав на байдарках с остановками для фотографирования и купания<br />
-              13:00 - обед на стоянке, отдых<br />
-              14:00 - сплав на байдарках с остановками для фотографирования и купания<br />
-              17:00 – прибытие на финиш<br />
-            </p>
-          </div>
-        </Col>
-      </Row>
+
+      <div className={container}>
+        <div className="container text">
+          <h3 style={{ textAlign: "center" }}>Примерная программа экспресс-сплава</h3>
+          <p style={{ fontSize: '18px', textAlign: "justify" }}>
+            10:00 – прибытие в р.п. Пронск в Глемпинг берег, трансфер на место старта.<br />
+            10:30 - инструктаж, получение снаряжение, загрузка байдарок, старт<br />
+            11:00 - сплав на байдарках с остановками для фотографирования и купания<br />
+            13:00 - обед на стоянке, отдых<br />
+            14:00 - сплав на байдарках с остановками для фотографирования и купания<br />
+            17:00 – прибытие на финиш<br />
+          </p>
+        </div>
+      </div>
 
 
-      <Row style={{ padding: '40px 0' }}>
+      {/*<Row style={{ padding: '40px 0' }}>
         <Col>
           <div className="container text">
             <Carousel interval={null}>
@@ -184,35 +239,36 @@ const kerd = () => {
             </Carousel>
           </div>
         </Col>
-      </Row>
+  </Row>*/}
 
+      <div className={container}>
+        <div className="container text">
+          <h3 style={{ textAlign: 'center' }}>Что взять с собой в поход</h3>
+          <b style={{ fontSize: '18px', textAlign: "justify" }}>💧 Питьевая вода</b>
+          <p style={{ fontSize: '18px', textAlign: "justify" }}>в удобной бутылке или нескольких маленьких</p>
+          <b style={{ fontSize: '18px', textAlign: "justify" }}>🧢 Запасная одежда</b>
+          <p style={{ fontSize: '18px', textAlign: "justify" }}>обувь в байдарку (сланцы, кроксы), головной убор, солнцезащитные очки</p>
+        </div>
+      </div>
 
-      <Row style={{ backgroundColor: '#eef0f2', padding: '40px 0' }}>
-        <Col>
-          <div className="container text">
-            <h3 style={{ textAlign: 'center' }}>Что взять с собой в поход</h3>
-            <b style={{ fontSize: '18px', textAlign: "justify" }}>💧 Питьевая вода</b>
-            <p style={{ fontSize: '18px', textAlign: "justify" }}>в удобной бутылке или нескольких маленьких</p>
-            <b style={{ fontSize: '18px', textAlign: "justify" }}>🧢 Запасная одежда</b>
-            <p style={{ fontSize: '18px', textAlign: "justify" }}>обувь в байдарку (сланцы, кроксы), головной убор, солнцезащитные очки</p>
-          </div>
-        </Col>
-      </Row>
+      <div className={container}>
+        <div className={videoContainer}>
+          <iframe className={video} src="https://www.youtube.com/embed/sQ2tOg3mrrs?si=SADGqVcuJLkjJrQI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen;" allowfullscreen></iframe>
+        </div>
+      </div>
 
-
-      <Row style={{ padding: '40px 0' }}>
-        <Col>
-          <div className="container text">
-            <Carousel controls={false} indicators={false}>
-              <Carousel.Item>
-                <div>
-                  <iframe width='100%' height='550' src="https://www.youtube.com/embed/sQ2tOg3mrrs?si=SADGqVcuJLkjJrQI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen;" allowfullscreen></iframe>
-                </div>
-              </Carousel.Item>
-            </Carousel>
-          </div>
-        </Col>
-      </Row>
+      <div className={footer} >
+        <div><img src={logoImage} className={logo} alt="Пронские сплавы логотип" /></div>
+        <div className={footerLinks} >
+          <a href="/splavy#routes">Маршруты</a>
+          <a href="/splavy/rent/">Аренда</a>
+          <a href="/splavy/company/">Организация корпоратива</a>
+        </div>
+        <div className={social}>
+          <a href="https://vk.com/pronskie_splavy" target='_blank' title='ВКонтакте'><img src={vk} alt='ВКонтакте' /></a>
+          <a href="https://ok.ru/group/70000002650473" target='_blank' title="Одноклассники"><img src={odnoklassniki} alt='Одноклассники' /></a>
+        </div>
+      </div>
     </Layout>
   )
 };
