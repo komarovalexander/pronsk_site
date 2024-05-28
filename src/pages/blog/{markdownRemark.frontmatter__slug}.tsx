0e43a095596e05e3eebee8ba3e8a38c682e01b13
Alexander Komarov <k.alex@qvedo.com>
2024-05-28 11:56:48 +0300

    add breadcrumb and fix styles

--- src/pages/blog/{markdownRemark.frontmatter__slug}.tsx
+++ src/pages/blog/{markdownRemark.frontmatter__slug}.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
 
+import {
+  breadcrumb,
+  breadcrumbItem,
+  container,
+  row
+} from '../splavy/splavy_detail.module.scss';
+
 import Layout from '../layout';
 import { graphql } from 'gatsby';
 
@@ -10,11 +17,22 @@ export default function BlogPostTemplate({
   const { frontmatter, html } = markdownRemark;
   return (
     <Layout>
-      <div className='container text'>
-        <div>
-          <h1>{frontmatter.title}</h1>
-          <p>{frontmatter.date}</p>
-          <div dangerouslySetInnerHTML={{ __html: html }} />
+
+      <div className={container} style={{ paddingTop: 0 }}>
+        <nav aria-label="breadcrumb" style={{ marginTop: 10 }}>
+          <ol className={breadcrumb}>
+            <li className={breadcrumbItem}><a href="/blog/">Блог</a></li>
+            <li className={breadcrumbItem}>/</li>
+          </ol>
+        </nav>
+      </div>
+      <div className={container} style={{ paddingTop: 0 }}>
+        <div className={row}>
+          <div>
+            <h1>{frontmatter.title}</h1>
+            <p>{frontmatter.date}</p>
+            <div dangerouslySetInnerHTML={{ __html: html }} />
+          </div>
         </div>
       </div>
     </Layout>
